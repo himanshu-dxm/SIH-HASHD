b72121dde3b5d920eef5999d6f30b7470084abfb
himanshu <himanhu.behl@gmail.com>
2022-03-22 14:01:30 +0530

    HomePage dart changed

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,19 +1,96 @@
-import 'package:flutter/material.dart';
+import 'dart:ui';
 
-class HomePage extends StatelessWidget {
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:hashd/widgets/common_styles.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text("HI")),
-      ),
-      body: Center(
-        child: Container(
-          child: Text("Hello"),
+      // appBar: AppBar(
+      //   // title: Center(child: Text("HI")),
+      // ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        alignment: Alignment.bottomCenter,
+        child: Stack(
+          children: [
+            Image.asset(
+              "assets/images/home_page_bg.jpg",
+              width: double.infinity,
+              height: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            ClipRect(
+              child: BackdropFilter(
+                filter: ImageFilter.blur(
+                  sigmaX: 2,
+                    sigmaY :2
+                ),
+                child: Container(
+                  height: MediaQuery.of(context).size.height,
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.symmetric(horizontal: 24),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Form(
+                        child: Column(
+                          children: [
+                            Text(
+                              "SIH-HASHD",
+                              style: TextStyle(
+                                color: Colors.white
+                              ),
+                            ),
+                            SizedBox(height: 35,),
+                            TextFormField(
+                              style: TextStyle(
+                                color: Colors.white
+                              ),
+                              maxLength: 16,
+                              keyboardType: TextInputType.number,
+                              decoration: CommonStyles.textFieldStyle("Enter Name"),
+                            ),
+                            SizedBox(height: 15,),
+                            TextFormField(
+                              style: TextStyle(
+                                  color: Colors.white
+                              ),
+                              maxLength: 16,
+                              keyboardType: TextInputType.number,
+                              decoration: CommonStyles.textFieldStyle("Enter Aadhaar Number"),
+                            ),
+                            SizedBox(height: 15,),
+                            TextFormField(
+                              style: TextStyle(
+                                  color: Colors.white
+                              ),
+                              maxLength: 16,
+                              keyboardType: TextInputType.number,
+                              decoration: CommonStyles.textFieldStyle("Enter Phone Number"),
+                            ),
+                            SizedBox(height: 15,),
+
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
         ),
-      ),
+      )
     );
   }
 }
