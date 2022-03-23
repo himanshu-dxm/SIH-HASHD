d4beacdcf5c07f7d6828cd907878f1b517a1cdef
himanshu <himanhu.behl@gmail.com>
2022-03-23 13:15:56 +0530

    Edited Home Page

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.cyan,
+        primarySwatch: Colors.green,
       ),
-      home: HomePage(),
+      home: LoginPage(),
     );
   }
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -5,8 +5,29 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("SIH HASHD"),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height*0.8,
+          alignment: Alignment.bottomCenter,
+          child: FloatingActionButton.extended(
+            onPressed: () {
+              print("Button Pressesd");
+            },
+              label: const Text(
+                "CAPTURE"
+              ),
+            icon: const Icon(
+              Icons.camera_alt_outlined
+            ),
+            // backgroundColor: Colors.green,
+          ),
+        ),
+      ),
     );
   }
 }
