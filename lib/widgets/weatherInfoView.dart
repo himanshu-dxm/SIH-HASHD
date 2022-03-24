ec428fa837e132e5003ddf2818bc3963a6dabc36
himanshu <himanhu.behl@gmail.com>
2022-03-24 12:11:08 +0530

    Integrated Weather in homePage

--- lib/widgets/weatherInfoView.dart
+++ lib/widgets/weatherInfoView.dart
@@ -20,6 +20,7 @@ class _WeatherInfoViewState extends State<WeatherInfoView> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: MediaQuery.of(context).size.height*0.25,
       margin: EdgeInsets.all(8),
       child: Center(
         child: Card(
@@ -28,29 +29,31 @@ class _WeatherInfoViewState extends State<WeatherInfoView> {
             // mainAxisSize: MainAxisSize.max,
             children: [
               ListTile(
+                trailing: Image.network("http://openweathermap.org/img/wn//${weather.iconUrl}@4x.png"),
                 // leading: Icon(Icons.album),
                 title: Text(
                   weather.city,
                 ),
                 subtitle: Text(
-                  weather.weatherType,
+                  "\nTemperature: "+weather.temperature+
+                      "\nWeather Type:"+weather.weatherType+"\nHumidity: "+weather.humidity.toString()+"\nPressure: "+weather.pressure.toString(),
                 ),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  TextButton(
-                    child: Text('Check Details'),
-                    onPressed: () {/* ... */},
-                  ),
-                  const SizedBox(width: 8),
-                  TextButton(
-                    child: Text('Delete'),
-                    onPressed: () {/* ... */},
-                  ),
-                  const SizedBox(width: 8),
-                ],
-              ),
+              // Row(
+              //   mainAxisAlignment: MainAxisAlignment.end,
+              //   children: [
+              //     TextButton(
+              //       child: Text('Check Details'),
+              //       onPressed: () {/* ... */},
+              //     ),
+              //     const SizedBox(width: 8),
+              //     TextButton(
+              //       child: Text('Delete'),
+              //       onPressed: () {/* ... */},
+              //     ),
+              //     const SizedBox(width: 8),
+              //   ],
+              // ),
             ],
           ),
         ),
